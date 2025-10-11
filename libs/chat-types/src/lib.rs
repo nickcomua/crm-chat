dea1010befc32dfcc8d078a66f055f4aebc53b95
nickp <mykola.korniichuk.ua@gmail.com>
2025-10-12 01:59:54 +0200

    add github workflow

---
+++ libs/chat-types/src/lib.rs
@@ -0,0 +1,42 @@
+use grammers_tl_types as tl;
+use serde::{Deserialize, Serialize};
+use surrealdb_types::{RecordId, SurrealValue};
+
+#[derive(Debug, Clone, Serialize, Deserialize, SurrealValue)]
+pub enum DBMessageContent {
+    Telegram(tl::enums::Message),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, SurrealValue)]
+pub struct DbMessage {
+    pub id: RecordId,
+    pub chat_id: RecordId,
+    pub client_id: String,
+    pub content: Vec<DBMessageContent>,
+    #[serde(default)]
+    pub deleted: bool,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum TgChat {
+    User(tl::enums::User),
+    Group(tl::enums::Chat),
+    Channel(tl::types::Channel),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DbChatContent {
+    Telegram(TgChat),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DbChat {
+    pub id: RecordId,
+    pub client_id: String,
+   pub content: Vec<DbChatContent>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Record {
+    pub id: RecordId,
+}
